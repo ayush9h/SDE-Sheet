bf018543b75ab3d22d14ee69eec17b277783d60e
Ayush Kumar <102464313+ayush9h@users.noreply.github.com>
2023-05-27 17:09:52 +0530

    Create jumpGame.cpp

---
+++ Binary Tree/jumpGame.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.size()==1)
+            return true;
+        if(nums[0]==0)
+            return false;
+        int dist=0;
+        
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==0 && dist==i)
+            {
+                return false;
+            }
+            dist=max(dist,nums[i]+i);
+            
+        }
+        return true;
+    }
+};
